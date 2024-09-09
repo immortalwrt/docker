c14019aae4f0e041bb1ffcabe1b4fd9eba474ea2
Tianling Shen <cnsztl@gmail.com>
2024-09-09 18:37:27 +0800

    download_files: add ua to bypass cf
    Signed-off-by: Tianling Shen <cnsztl@gmail.com>

--- download_files.sh
+++ download_files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright (C) 2021 ImmortalWrt.org
 
+CURL_UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+
 DOWNLOAD_URL="https://immortalwrt.kyarucloud.moe"
 if [ "$VERSION" == "snapshot" ]; then
 	DOWNLOAD_PATH="snapshots"
@@ -11,7 +13,7 @@ DOWNLOAD_PATH+="/targets/$(echo "$TARGET" | tr "-" "/")"
 
 function verify_shasum(){
 	[ -n "$1" ] || exit 1
-	curl --retry 3 --retry-all-errors --retry-delay 10 -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep "$1" > "$1.sha256sums"
+	curl -A "$CURL_UA" --retry 3 --retry-all-errors --retry-delay 10 -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep "$1" > "$1.sha256sums"
 	LOCAL_HASH="$(sha256sum $1 | awk '{print $1}')"
 	REMOTE_HASH="$(awk '{print $1}' "$1.sha256sums")"
 	if [ "$LOCAL_HASH" == "$REMOTE_HASH" ]; then
@@ -27,23 +29,23 @@ function verify_shasum(){
 
 case "$1" in
 "ib")
-	IB_NAME="$(curl -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep -E "imagebuilder-(.*)${TARGET%-*}" | cut -d "*" -f 2)"
-	curl --retry 3 --retry-all-errors --retry-delay 10 -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$IB_NAME"
+	IB_NAME="$(curl -A "$CURL_UA" -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep -E "imagebuilder-(.*)${TARGET%-*}" | cut -d "*" -f 2)"
+	curl --retry 3 --retry-all-errors --retry-delay 10 -A "$CURL_UA" -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$IB_NAME"
 	verify_shasum "$IB_NAME"
 	mkdir -p "ib"
 	tar -vxf "$IB_NAME" -C "ib"/ --strip-components 1
 	;;
 "rootfs")
-	ROOTFS_NAME="$(curl -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep "\-rootfs.tar.gz" | cut -d "*" -f 2)"
-	curl --retry 3 --retry-all-errors --retry-delay 10 -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$ROOTFS_NAME"
+	ROOTFS_NAME="$(curl -A "$CURL_UA" -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep "\-rootfs.tar.gz" | cut -d "*" -f 2)"
+	curl --retry 3 --retry-all-errors --retry-delay 10 -A "$CURL_UA" -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$ROOTFS_NAME"
 	verify_shasum "$ROOTFS_NAME"
 	mkdir -p "rootfs"
 	tar -vxf "$ROOTFS_NAME" -C "rootfs"/ --strip-components 1
 	cp -fpR "rootfs_extra"/* "rootfs"/
 	;;
 "sdk")
-	SDK_NAME="$(curl -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep -E "sdk-(.*)${TARGET%-*}" | cut -d "*" -f 2)"
-	curl --retry 3 --retry-all-errors --retry-delay 10 -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$SDK_NAME"
+	SDK_NAME="$(curl -A "$CURL_UA" -fsSL "$DOWNLOAD_URL/$DOWNLOAD_PATH/sha256sums" | grep -E "sdk-(.*)${TARGET%-*}" | cut -d "*" -f 2)"
+	curl --retry 3 --retry-all-errors --retry-delay 10 -A "$CURL_UA" -fLO "$DOWNLOAD_URL/$DOWNLOAD_PATH/$SDK_NAME"
 	verify_shasum "$SDK_NAME"
 	mkdir -p "sdk"
 	tar -vxf "$SDK_NAME" -C "sdk"/ --strip-components 1
